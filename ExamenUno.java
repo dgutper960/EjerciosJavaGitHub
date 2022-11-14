735777ae160cd22376e48996fd36e4f58f41bcfb
dgutper960 <115797153+dgutper960@users.noreply.github.com>
2022-11-14 12:12:29 +0100

    Lo visto en la clase 14 noviembre

---
+++ ExamenUno.java
@@ -0,0 +1,14 @@
+public class ExamenUno {
+//Dado un número n, escribir la suma de todos los mútiplos de 3 y 5 hasta n
+public static void main(String[] args) {
+    final int n1 = 3;
+    final int n2 = 5;
+    int n = 45;
+    
+    for(int i=n1; i<=n; i++ ){
+        if(i%n1!=0);
+        System.out.println(i);
+    }
+}
+
+}
